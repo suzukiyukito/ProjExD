95861263133b5af6e145711270db0d95ecdd5700
Suzukui <c0b210956a@edu.teu.ac.jp>
2022-09-27 17:38:53 +0900

    ぬーーーーん

---
+++ ex01/a.py
@@ -0,0 +1,21 @@
+import random,time 
+def shutudai():
+    qst =[["勤労感謝の日は何月何日？",("11月23日","１１月２３日","11がつ23にち","11月２３日","１１月23日")],
+    ["海の日は何月何日？",("7月18日","７月１８日","7がつ18にち","7月１８日","７月18日")],
+    ["憲法記念日は何月何日？",("5月7日","５月７日","5がつ7にち","5月７日","５月7日")]]
+    num = random.randint(0,len(qst)-1)
+    print(qst[num][0])
+    return qst[num][1]
+
+def kaito(n):
+    t1 = time.time()
+    ans = input()
+    t2 = time.time()
+    if ans in n:
+        print("正解！")
+    else:
+        print("はずれ～～～～")
+    print(f"タイム:{t2-t1}秒")
+if __name__ == "__main__":
+    n = shutudai()
+    kaito(n)
