e61420691bac7db724eddf1dd5bd803540946ce3
Suzukui <c0b210956a@edu.teu.ac.jp>
2022-10-04 14:37:28 +0900

    1

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,5 +1,11 @@
 import tkinter as tk
-
+import tkinter.messagebox as tkm
 root = tk.Tk()
 root.geometry("300x500")
+for i in range(4):
+    for j in range(3):
+        num = 9-(j+i*3)
+        if num >= 0:
+            button = tk.Button(root,text=num,font=("Times New Roman",30),width=4,height=2)
+            button.grid(column=j,row =i)
 root.mainloop()
