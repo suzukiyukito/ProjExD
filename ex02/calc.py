9de71fe78f2c266b63757949c46dc347ee1dedbc
Suzukui <c0b210956a@edu.teu.ac.jp>
2022-10-04 14:53:35 +0900

    2

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,11 +1,20 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
+
+def button_click(event):
+    a = event.widget
+    txt = a["text"]
+    tkm.showinfo(txt,f"{txt}ボタンが押されました")
+
 root = tk.Tk()
 root.geometry("300x500")
 for i in range(4):
     for j in range(3):
         num = 9-(j+i*3)
         if num >= 0:
-            button = tk.Button(root,text=num,font=("Times New Roman",30),width=4,height=2)
+            button = tk.Button(root,text=num,font=("Times New Roman",30),width=4,height=2,command=button_click)
+            button.bind("<1>",button_click)
             button.grid(column=j,row =i)
+
+
 root.mainloop()
