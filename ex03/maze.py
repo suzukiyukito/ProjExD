79f21103e0099cec6fa8ce316d11a50fdce5c2ec
Suzukui <c0b210956a@edu.teu.ac.jp>
2022-10-18 17:15:28 +0900

    完成版

--- ex03/maze.py
+++ ex03/maze.py
@@ -5,18 +5,32 @@ from maze_maker import *
 from PIL import Image,ImageTk
 
 import sys
+
+import time
+
 key = ""
 def key_down(event):#キーが押された時に呼びだされる関数
     global key
     key = event.keysym
-   
+
+    if key == "Escape":
+        messagebox.showinfo("なんや","ようこんな機能があるのに気づいたな")
+        ask = messagebox.askquestion("いいのか？","ゴールしちまうけどええんか？")
+        print(ask)
+        if ask =="yes":
+            ret = messagebox.showinfo("ゴール",f"おめでとう\n君はズルをした・.・")
+            print(ret)
+            if ret == "ok":
+                sys.exit()
 
 def key_up(event):
     global key
     key =""
+
+
   
 def main_proc():
-    global cx,cy,mx,my,jid
+    global cx,cy,mx,my,jid,mz
     if key == "Up" and (mz[my-1][mx] == 0 or mz[my-1][mx] ==2):
         cy -= 100
         my -= 1
@@ -30,24 +44,28 @@ def main_proc():
         cx += 100
         mx += 1
 
+        
+
     canvas.coords(t,cx,cy)
     jid = root.after(100,main_proc)
-
+    #ゴールに到達したときの処理
     if mz[my][mx] == mz[7][13]:
         root.after_cancel(jid)
+        t2 = time.time()
         jid = None
-        pop_message()
+        pop_message(t2)
     
 def start_end():
     canvas.create_rectangle(100, 100, 200, 200, fill="red")   
     canvas.create_rectangle(1300, 700, 1400, 800, fill="green")   
 
 #メッセージ表示
-def pop_message():
-    ret = messagebox.showinfo("ゴール","おめでとうございます")
-    if ret == True:
-        sys.exit()
+def pop_message(time):
+    global t1
 
+    ret = messagebox.showinfo("ゴール",f"おめでとう\nクリアタイムは{int(time-t1)}秒やで")
+    if ret == "ok":
+        sys.exit()
 
 if __name__ == "__main__":
     cx,cy = 150,150
@@ -86,7 +104,7 @@ if __name__ == "__main__":
     tori = tk.PhotoImage(file="./ex03/fig/3.png")
     t = canvas.create_image(cx,cy,image=tori,tag="tori")
     canvas.pack()
-    
+    t1 = time.time()
     main_proc()
     #キーが入力された時に反応する
     root.bind("<KeyPress>",key_down)
