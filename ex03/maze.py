c71b03c6710938cef91965da8718ce08dba9c626
Suzukui <c0b210956a@edu.teu.ac.jp>
2022-10-18 14:45:20 +0900

    とりまおわり

--- ex03/maze.py
+++ ex03/maze.py
@@ -3,7 +3,6 @@ import tkinter as tk
 from maze_maker import *
 
 key = ""
-cx,cy = 300,400
 def key_down(event):#キーが押された時に呼びだされる関数
     global key
     key = event.keysym
@@ -14,21 +13,27 @@ def key_up(event):
     key =""
   
 def main_proc():
-    global cx,cy
-    if key == "Up":
-        cy -= 20
-    elif key == "Down":
-        cy += 20
-    elif key == "Left":
-        cx -= 20
-    elif key == "Right":
-        cx += 20
+    global cx,cy,mx,my
+    if key == "Up" and mz[my-1][mx] == 0:
+        cy -= 100
+        my -= 1
+    elif key == "Down"and mz[my+1][mx]  == 0:
+        cy += 100
+        my += 1
+    elif key == "Left"and mz[my][mx-1]  == 0:
+        cx -= 100
+        mx -= 1
+    elif key == "Right"and mz[my][mx+1] == 0:
+        cx += 100
+        mx += 1
     canvas.coords(t,cx,cy)
-    root.after(50,main_proc)
+    root.after(100,main_proc)
     
 
 
 if __name__ == "__main__":
+    cx,cy = 150,150
+    mx,my = 1,1
     root = tk.Tk()
     root.title("迷えるこうかとん")
     root.geometry("1500x900")
@@ -41,17 +46,19 @@ if __name__ == "__main__":
     )
 
     mz = make_maze(15,9)
+    print(mz)
     show_maze(canvas,mz)
 
     tori = tk.PhotoImage(file="./ex03/fig/3.png")
     t = canvas.create_image(cx,cy,image=tori,tag="tori")
     canvas.pack()
 
+    main_proc()
     #キーが入力された時に反応する
     root.bind("<KeyPress>",key_down)
     #キーが話された時に反応する
     root.bind("<KeyRelease>",key_up)
 
-    main_proc()
+    
 
     root.mainloop()
